bfee04f9c788aca8c9d0f8ebca5682d8eb1da97a
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-11-30 17:02:08 +0100

    creation griffon

--- src/Griffin.ts
+++ src/Griffin.ts
@@ -1,7 +1,24 @@
+import { Character } from "./Character";
 import { Enemy } from "./enemy";
+import { Hero } from "./Hero";
 
 export class griffin extends Enemy {
-    constructor(parameters) {
-        
+    constructor(name: string, health: number, strength: number) {
+        super(name, health, strength)
     }
+
+    setDamage(damage: number): void {
+        this.setHealth(this.getHealth() - damage * 0.1);
+    }
+    
+        
+    attack(opponent:Character){
+        if(this.flying){
+       opponent.setHealth(opponent.getHealth() - this.getStrenght() *0.1);
+    }else{
+    super.attack(opponent)
+       
+    }  
+   
+}
 }
