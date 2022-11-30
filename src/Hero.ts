e9f29472cbc7b2bd4673e5c18a0717b5961e207f
Wyllwyll <terry.boeuf@hotmail.fr>
2022-11-30 15:20:07 +0100

    changement class race en string dans Hero

--- src/Hero.ts
+++ src/Hero.ts
@@ -15,5 +15,32 @@ export class Hero extends Character {
     setRace(race: string) {
         this.race = race
     }
-}
 
+    attack(opponent: Character): void {
+        switch (this.race) {
+            case "Elf": {
+                if (opponent.flying) {
+                    opponent.setHealth(opponent.getHealth() - this.getStrenght() * 1.10);
+                }
+                else {
+                    opponent.setHealth(opponent.getHealth() - this.getStrenght() * 0.9);
+
+                }
+                break
+            }
+            case "Human": {
+                if (opponent.flying) {
+                    opponent.setHealth(opponent.getHealth() - this.getStrenght() * 0.9);
+                }
+                else {
+                    opponent.setHealth(opponent.getHealth() - this.getStrenght() * 1.10);
+                }
+                break
+            }
+            case "Dwarf": {
+                opponent.setHealth(opponent.getHealth() - this.getStrenght());
+                break
+            }
+        }
+    }
+}
