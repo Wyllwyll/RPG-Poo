5a3ddf5e0cd776ac2779be9e9f790d2dfdc28579
Wyllwyll <terry.boeuf@hotmail.fr>
2022-11-30 16:46:43 +0100

    maj

--- src/Berserker.ts
+++ src/Berserker.ts
@@ -1,7 +1,12 @@
 import { Enemy } from "./enemy";
 
 export class Berserker extends Enemy {
-    constructor(parameters) {
-        
+    constructor(name: string, health: number, strengt: number) {
+        super(name, health, strengt)
     }
+
+    setDamage(damage: number): void {
+        this.setHealth(this.getHealth() - damage * 0.7);
+    }
+
 }
