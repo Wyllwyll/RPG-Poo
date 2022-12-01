81efc65fc940268d6d82e093bdeeb1a1635eded5
Wyllwyll <terry.boeuf@hotmail.fr>
2022-12-01 16:59:31 +0100

    battle finis

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@
 import { Assassin } from "./Assassin"
 import { Battle } from "./Bataille"
 import { Berserker } from "./Berserker"
+import { Dragon } from "./Dragon"
 import { Golem } from "./Golem"
 import { Hero } from "./Hero"
 import { Werewolf } from "./Werewolf"
 
 
-const So = new Hero("So", 100, 20, "Elf")
+const So = new Hero("So", 100, 15, "Elf")
 
-const croissant = new Werewolf("croissant", 80, 10)
+const croissant = new Dragon("croissant", 200, 30)
 
 const batailleSimplon = new Battle(So,croissant)
 
