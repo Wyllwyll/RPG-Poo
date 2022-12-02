82c17f7cc1b98683b7c3aef4606f4337508f396b
Wyllwyll <terry.boeuf@hotmail.fr>
2022-12-02 11:49:42 +0100

    creation des heros enemies

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,51 @@ import { Battle } from "./Bataille"
 import { Berserker } from "./Berserker"
 import { Dragon } from "./Dragon"
 import { Golem } from "./Golem"
+import { griffin } from "./Griffin"
 import { Hero } from "./Hero"
 import { Werewolf } from "./Werewolf"
 
 
-const So = new Hero("So", 100, 15, "Elf")
 
-const croissant = new Dragon("croissant", 200, 30)
 
-const batailleSimplon = new Battle(So,croissant)
+const humain = new Hero("humainTest", 100, 15, "Human")
+const elf = new Hero("elfTest", 100, 15, "Elf")
+const nain = new Hero("testDwarf", 90, 15, "Dwarf")
+
+let heroPlayer:any []=[]
+
+const selectHero = document.getElementById("selectHeros")
+
+selectHero.addEventListener("change", function (e) {
+    console.log(e);
+    const who=(e.target as HTMLInputElement).value
+    heroPlayer.push(who)
+    console.log(heroPlayer);
+})
+
+
+
+
+
+
+
+const drake = new Dragon("TestDragon", 200, 30)
+const gol = new Golem("testGolem", 150, 20)
+const ass = new Assassin("testAssassin", 100, 15)
+const bers = new Berserker("testBerserker", 110, 20)
+const grif = new griffin("testGriffon", 170, 25)
+const loup = new Werewolf("testWerewolf", 170, 20)
+
+
+let enemyPlayer:any []=[]
+
+
+
+
+
+
+
+const batailleSimplon = new Battle(humain, drake)
 
 
 batailleSimplon.toDeath()
